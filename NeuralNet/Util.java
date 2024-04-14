71392e35e0b80f913e44074462d1dffe69e6c73f
huwbk <124587812+huwbk@users.noreply.github.com>
2024-04-14 21:08:18 +0000

    Completion of previous change, to inclue Util.java

--- NeuralNet/Util.java
+++ NeuralNet/Util.java
@@ -1,10 +1,18 @@
+/**
+ * Class containing useful methods for mathematical processing, used in the training and use of Layer.java and Network.java.
+ */
+
 import java.util.*;
 import java.util.concurrent.ThreadLocalRandom;
-
 import org.ejml.simple.*;
 
 public class Util {
     
+    /**
+     * Applies a sigmoid function to all of the values in a matrix.
+     * @param input A matrix.
+     * @return A matrix, with sigmoid function applied.
+     */
     public static SimpleMatrix sigmoid (SimpleMatrix input) {
         for (int i = 0; i < input.getNumRows(); i++) {
             for (int j = 0; j < input.getNumCols(); j++) {
@@ -14,6 +22,13 @@ public class Util {
         return input;
     }
 
+    /**
+     * Calculates the mean square loss from two lists of vectors.
+     * i.e. Finds the difference between each pair of values and squares it. Then, finds the mean result across all pairs.
+     * @param correctAnswers one list of vectors.
+     * @param predictedAnswers the other list of vectors.
+     * @return the mean square loss
+     */
     public static Double meanSquareLoss (List<SimpleMatrix> correctAnswers, List<SimpleMatrix> predictedAnswers) {
         Double sumSquare = 0d;
 
@@ -24,6 +39,14 @@ public class Util {
         return sumSquare / (correctAnswers.size());
     }
 
+    /**
+     * Fills a matrix with random values.
+     * @param rows Number of rows in desired matrix.
+     * @param cols Number of columns in desired matrix.
+     * @param minimum Lower bound for generation of random values.
+     * @param maximum Upper bound for generation of random values.
+     * @return Randomised Matrix.
+     */
     public static SimpleMatrix randomiseMatrix(int rows, int cols, Double minimum, Double maximum) {
         SimpleMatrix randomMatrix = new SimpleMatrix(rows, cols);
         for (int i = 0; i < rows; i++) {
@@ -32,14 +55,5 @@ public class Util {
             }
         }
         return randomMatrix;
-    }
-
-    public static void printMatrix(SimpleMatrix matrix) {
-        for (int i = 0; i < matrix.getNumRows(); i++) {
-            for (int j = 0; j < matrix.getNumCols(); j++) {
-                System.out.print(matrix.get(i, j) + " ");
-            }
-            System.out.println();
-        }
     }
 }

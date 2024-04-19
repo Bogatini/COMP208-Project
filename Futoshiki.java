196fd5336792315d4c76d8535a5d3ed8b1114927
huwbk <124587812+huwbk@users.noreply.github.com>
2024-04-19 11:08:58 +0100

    fixed second / milisecond confusion

--- Futoshiki.java
+++ Futoshiki.java
@@ -525,7 +525,7 @@ public class Futoshiki{
 				
 			timer.stop();
 			double timeDifferenceDouble = (double) timeDifference;
-			timeDifferenceDouble = timeDifferenceDouble / 1000;
+			// timeDifferenceDouble = timeDifferenceDouble / 1000; // i think this is already in seconds ? - huw
 			NNInterface.addTrainingData(gameAsArray, timeDifferenceDouble);
 			
 			NNInterface.save();

cf53fe700e847c1f432b8a75f26ca90894aa5852
Bogatini <86053070+Bogatini@users.noreply.github.com>
2024-04-20 10:03:53 +0100

    Made done button bigger

--- Maze/MazeCreator.java
+++ Maze/MazeCreator.java
@@ -16,9 +16,10 @@ public class MazeCreator extends JFrame {
 
     /**
      * Constructor that creates the input window for the user
-     * @param   rows number of rows in the grid
-     * @param   columns number of columns in the grid
-    */
+     *
+     * @param rows    number of rows in the grid
+     * @param columns number of columns in the grid
+     */
     public MazeCreator(int rows, int columns) {
         this.rows = rows;
         this.columns = columns;
@@ -26,7 +27,7 @@ public class MazeCreator extends JFrame {
         gridButtons = new JButton[rows][columns];
 
         setTitle("Maze Creator");
-        setLayout(new GridLayout(rows + 1, columns)); // +1 for the print button row - annoyingly this means the print button is a square (cant find a way to make it not stick to the grid payout set in this line)
+        setLayout(new GridBagLayout()); // Change layout to GridBagLayout
         initializeGrid();
         addPrintButton();
         setSize(columns * CELL_SIZE, (rows + 1) * CELL_SIZE); // Adjust size based on number of rows and columns
@@ -35,13 +36,18 @@ public class MazeCreator extends JFrame {
     }
 
     /**
-     * Creates an empty grid of buttons the user can click on to draw a maze
-     * Clicking a button changes it appearance and reflects this change in the array representing the maze
-     * "W" is a wall represented in black
-     * " " is an empty space represented in white
-     * Three mouse listener functions are used here to allow the user to click and drag to select buttons
-    */
+     * Creates an empty grid of buttons the user can click on to draw a maze Clicking a button changes it appearance
+     * and reflects this change in the array representing the maze "W" is a wall represented in black " " is an empty
+     * space represented in white Three mouse listener functions are used here to allow the user to click and drag to
+     * select buttons
+     */
     private void initializeGrid() {
+        GridBagConstraints gridSettings = new GridBagConstraints();
+        gridSettings.fill = GridBagConstraints.BOTH; // Make buttons stretch in both directions
+        gridSettings.weightx = 1.0 / columns; // Evenly distribute horizontal space
+        gridSettings.weighty = 1.0 / (rows + 1); // Evenly distribute vertical space including the row for the "Done" button
+
+
         for (int y = 0; y < rows; y++) {
             for (int x = 0; x < columns; x++) {
                 MAZE[y][x] = " ";
@@ -57,8 +63,7 @@ public class MazeCreator extends JFrame {
                         if (e.getButton() == MouseEvent.BUTTON1) {
                             MAZE[finalY][finalX] = "W";
                             button.setBackground(Color.BLACK);
-                        } 
-                        else if (e.getButton() == MouseEvent.BUTTON3) {
+                        } else if (e.getButton() == MouseEvent.BUTTON3) {
                             MAZE[finalY][finalX] = " ";
                             button.setBackground(Color.WHITE);
                         }
@@ -69,8 +74,7 @@ public class MazeCreator extends JFrame {
                         if (SwingUtilities.isLeftMouseButton(e)) {
                             MAZE[finalY][finalX] = "W";
                             button.setBackground(Color.BLACK);
-                        }
-                        else if (SwingUtilities.isRightMouseButton(e)) {
+                        } else if (SwingUtilities.isRightMouseButton(e)) {
                             MAZE[finalY][finalX] = " ";
                             button.setBackground(Color.WHITE);
                         }
@@ -81,24 +85,30 @@ public class MazeCreator extends JFrame {
                         if (SwingUtilities.isLeftMouseButton(e)) {
                             MAZE[finalY][finalX] = "W";
                             button.setBackground(Color.BLACK);
-                        }
-                        else if (SwingUtilities.isRightMouseButton(e)) {
+                        } else if (SwingUtilities.isRightMouseButton(e)) {
                             MAZE[finalY][finalX] = " ";
                             button.setBackground(Color.WHITE);
                         }
                     }
                 });
                 gridButtons[y][x] = button;
-                add(button);
+                gridSettings.gridx = x;
+                gridSettings.gridy = y;
+                add(button, gridSettings);
             }
         }
     }
 
     /**
      * Adds a button at the bottom of the maze that lets the user finish editing the maze
-    */
+     */
     private void addPrintButton() {
-        JButton printButton = new JButton("Finish Maze");
+        GridBagConstraints gridSettings = new GridBagConstraints();
+        gridSettings.fill = GridBagConstraints.BOTH; // Make button stretch in both directions
+        gridSettings.gridwidth = columns; // Make the button span all columns
+        gridSettings.gridx = 0;
+        gridSettings.gridy = rows; // Place the button on the row below the grid
+        JButton printButton = new JButton("Done");
         printButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -106,22 +116,24 @@ public class MazeCreator extends JFrame {
                 dispose(); // close the window
             }
         });
-        add(printButton);
+        add(printButton, gridSettings);
     }
 
     public String[][] getMaze() {
         return MAZE;
     }
+
     public boolean getContinueFlag() {
         return continueFlag;
     }
-    public void setContinueFlag(boolean inputFlag){
+
+    public void setContinueFlag(boolean inputFlag) {
         this.continueFlag = inputFlag;
     }
 
-    private void printMaze() {
-        for (int i = 0; i< MAZE.length; i++) {
-            for (int j = 0; j< MAZE[i].length; j++) {
+    public void printMaze() {
+        for (int i = 0; i < MAZE.length; i++) {
+            for (int j = 0; j < MAZE[i].length; j++) {
                 System.out.print(MAZE[i][j] + ", ");
             }
             System.out.print("\n");
